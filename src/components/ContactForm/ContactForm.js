da3cc476a9b5fe40ab111e6528ecd99d11988191
Ihor Nadieien <ingwar1802@gmail.com>
2020-12-12 10:00:24 +0200

    add phonebook

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,98 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import s from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  };
+
+  contactMatching = () => {
+    const { name, number } = this.state;
+    const { contacts } = this.props;
+    const namesInPhonebook = contacts.reduce(
+      (acc, contact) => [...acc, contact.name],
+      [],
+    );
+    const numbersInPhonebook = contacts.reduce(
+      (acc, contact) => [...acc, contact.number],
+      [],
+    );
+
+    if (
+      namesInPhonebook.includes(name) ||
+      numbersInPhonebook.includes(number)
+    ) {
+      alert(`${name}${number} is already in contacts`);
+      return true;
+    }
+
+    if (name === '' || number === '') {
+      alert('Please enter all data');
+      return true;
+    }
+  };
+
+  handleSubmit = e => {
+    const { name, number } = this.state;
+
+    e.preventDefault();
+    this.setState({ name: '', number: '' });
+    if (this.contactMatching()) {
+      return;
+    }
+
+    this.props.onSubmit(name, number);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className={s.form}>
+        <label className={s.label}>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            placeholder="Jack Sparrow"
+            onChange={this.handleChange}
+            className={s.input}
+          />
+        </label>
+
+        <label className={s.label}>
+          Number
+          <input
+            type="tel"
+            name="number"
+            value={this.state.number}
+            placeholder="111-11-11"
+            onChange={this.handleChange}
+            className={s.input}
+          />
+        </label>
+        <button type="submit" className={s.button}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+export default ContactForm;
